a8ce233a019074752bf7c6ad285ec5e530712e4c
firepaperbird <thanvaquy1996@gmail.com>
2018-03-01 23:13:42 +0700

    working with login page

---
+++ js/login-page.js
@@ -0,0 +1,8 @@
+function addstyle()
+      {
+        document.getElementById('style').href='css/login.css';
+      }
+function goToByScroll(){
+      // Scroll
+    $('html,body').animate({ scrollTop: 100} ,'slow');
+}
