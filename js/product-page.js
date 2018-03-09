72da2d44d1c2cb0ccf153fee5e1910f0570bbd20
Nguyễn Thúy Ngọc <ngocnt1801@gmail.com>
2018-03-09 22:27:59 +0700

    load list product from server

--- js/product-page.js
+++ js/product-page.js
@@ -11,4 +11,73 @@ function AddListenerForMenu() {
 }
 $(document).ready(function () {
     AddListenerForMenu();
+    GetAllProduct();
 })
+
+function GetAllProduct(){
+    var dataJSON ={
+        sortByPrice:-1, //sort from newest to oldest
+        sortById:0
+    };
+    var request = jQuery.ajax({
+        type:"GET",
+        url: HOST + "product",
+        dataType:'json',
+        data:dataJSON,
+        header: {"Access-Control-Allow-Origin":true},
+        traditional: true
+    });
+    request.done(function (data) {
+        console.log(data);
+        CreateListItem(data);
+    });
+    request.fail(function (data) {
+       console.log("fail roi");
+    });
+}
+
+function  CreateListItem(products) {
+    var list = $("#list-item-product");
+    products.forEach(function (product,index) {
+        console.log(product);
+        list.append(CreateItemProduct(product));
+    });
+}
+
+function CreateItemProduct(product) {
+    var item = $("<div class='item'></div>");
+    var itemImage = CreateImage(product.Url,product.Name);
+    var description = $('<div class="item-description"></div>');
+    var itemName = $('<p class="item-name"></p>');
+    itemName.append(product.Name);
+    var divPrice = $('<div></div>');
+    var currentPice = $('<span class="price"></span>');
+    var price = (product.Type == false)? (product.Price*(1-product.Discount/100)):(product.Price-product.Discount);
+    currentPice.append(price + "đ   ");
+
+    divPrice.append(currentPice);
+    if (product.Discount != 0){
+        var deletedPrice=$('<span class="delete-price"></span>');
+        deletedPrice.append(product.Price + "đ   ");
+        divPrice.append(deletedPrice);
+    }
+
+    description.append(itemName);
+    description.append(divPrice);
+    item.append(itemImage);
+    item.append(description);
+    return item;
+
+
+}
+function CreateImage(src, name) {
+    var itemImage = $('<div class="item-image"></div>');
+    var image = $("<img/>");
+    if (src === ''){
+        src = 'img/No_Image_Available.jpg';
+    }
+    image.attr('src',src);
+    image.attr('alt',name);
+    itemImage.append(image);
+    return itemImage;
+}
